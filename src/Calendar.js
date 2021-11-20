260ee8b16c9852d21bbae10227697efe202568e3
Natalie Hewison <nataliehewison@hotmail.co.uk>
2021-11-20 17:56:39 +0000

    created open door component

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,10 +1,13 @@
 import { ClosedDoor } from './components/ClosedDoor'
 import './Calendar.css';
+import { OpenDoor } from './components/OpenDoor';
 
 function Calendar() {
   return (
     <div className="calendar">
       <ClosedDoor />
+      <OpenDoor />
+  
     </div>
   );
 }
---
+++ src/components/OpenDoor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './OpenDoor.css';
+
+export const OpenDoor = () => {
+    return (
+        <div className='open-door'>image go here
+            <img alt='' src=''></img>
+        </div>
+    )
+};

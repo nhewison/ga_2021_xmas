f42d66bf80bee7881e4e5323d4d2c543e90edf2f
Natalie Hewison <nataliehewison@hotmail.co.uk>
2021-11-20 18:12:25 +0000

    door modal container and door details box created

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,13 +1,14 @@
 import { ClosedDoor } from './components/ClosedDoor'
 import './Calendar.css';
 import { OpenDoor } from './components/OpenDoor';
+import { DoorDetailsModal } from './components/DoorDetailsModal';
 
 function Calendar() {
   return (
     <div className="calendar">
       <ClosedDoor />
       <OpenDoor />
-  
+      <DoorDetailsModal />
     </div>
   );
 }
---
+++ src/components/DoorDetailsModal.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DoorDetailsModal.css';
+
+export const DoorDetailsModal = () => {
+    return (
+        <div className='door-details-container'>
+           <div className='door-details'>  
+               <img alt='image-here' src=''></img>
+                <p>quote here</p>
+                <p>name</p>
+            </div>
+        </div>
+    )
+};

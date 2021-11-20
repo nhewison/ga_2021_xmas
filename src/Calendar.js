74431825f9f1d2644d18a343726dee2921334f0d
Natalie Hewison <nataliehewison@hotmail.co.uk>
2021-11-20 17:38:58 +0000

    background added and closed door started

---
+++ src/Calendar.js
@@ -0,0 +1,12 @@
+import { ClosedDoor } from './components/ClosedDoor'
+import './Calendar.css';
+
+function Calendar() {
+  return (
+    <div className="calendar">
+      <ClosedDoor />
+    </div>
+  );
+}
+
+export default Calendar;
---
+++ src/components/ClosedDoor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ClosedDoor.css';
+
+export const ClosedDoor = () => {
+    return (
+        <div className='closed-door'>
+            <div className='door-number'>number</div>
+        </div>
+    )
+};

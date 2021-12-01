0b6b60f3e01235cc04bebe604f15ad1a6b37fa25
Natalie Hewison <nataliehewison@hotmail.co.uk>
2021-12-01 21:24:56 +0000

    door data added and fonts updated

--- src/components/DoorDetailsModal.js
+++ src/components/DoorDetailsModal.js
@@ -6,7 +6,7 @@ export const DoorDetailsModal = ( {data, onClick } ) => {
         <div className='door-details-container' onClick={onClick} >
            <div className='door-details'>  
                <img alt='image-here' src={data.img}></img>
-                <p>{data.text}</p>
+                <p className='modal-quote'>{data.text}</p>
                 <p className='modal-name'>{data.name}</p>
             </div>
         </div>
